269cc1b0ee0b342574c9f220b7180750e67abd7c
Jarryd Trokis <jarryd.jay@gmail.com>
2025-06-17 07:18:50 +0100

    ios working

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,157 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+};
+
+export const OnsColors = {
+  // Grey palette
+  onsColorBlack: "#222",
+  onsColorGrey100: "#414042",
+  onsColorGrey75: "#707071",
+  onsColorGrey35: "#bcbcbd",
+  onsColorGrey25: "#d5d5d6",
+  onsColorGrey15: "#e2e2e3",
+  onsColorGrey10: "#efeff0",
+  onsColorGrey5: "#f5f5f6",
+  onsColorWhite: "#fff",
+
+  // Primary brand palette
+  onsColorNightBlue: "#003c57", // ONS logo primary
+  onsColorSpringGreen: "#a8bd3a", // ONS logo accent
+  onsColorOceanBlue: "#206095",
+  onsColorSkyBlue: "#27a0cc",
+  onsColorAquaTeal: "#00a3a6",
+  onsColorNavyBlue: "#194766",
+  onsColorPaleBlue: "#e7eff4",
+  onsColorSoftBlueGrey: "#dee7ee",
+  onsColorLightBlueGrey: "#e9eff4",
+
+  // Supporting palette
+  onsColorLeafGreen: "#0f8243",
+  onsColorRubyRed: "#d0021b",
+  onsColorJaffaOrange: "#fa6401",
+  onsColorSunYellow: "#fbc900",
+  onsColorNeonYellow: "#f0f762",
+  onsColorLightOrange: "#e8bb9b",
+
+  // Core colour adjustments - tints
+  onsColorOceanBlueTint: "#e9eff4",
+  onsColorOceanBlueVibrant: "#1f8fd8",
+  onsColorSpringGreenTint: "#c3c5b8",
+  onsColorLeafGreenTint: "#e7f3ec",
+  onsColorLeafGreenVibrant: "#10ca64",
+  onsColorLeafGreenDark10: "#073d20",
+  onsColorLeafGreenDark5: "#0c6b37",
+  onsColorRubyRedTint: "#fae6e8",
+  onsColorRubyRedVibrant: "#fd112d",
+  onsColorJaffaOrangeTint: "#fff0e6",
+  onsColorJaffaOrangeVibrant: "#ff7b24",
+  onsColorSunYellowDark: "#e2b500",
+  onsColorNavyBlueLight: "#153b55",
+
+  // Theme branding
+  onsColorBranded: "#206095", // ocean blue
+  onsColorBrandedText: "#206095", // ocean blue
+  onsColorBrandedTint: "#e9eff4", // ocean blue tint
+  onsColorBrandedSecondary: "#003c57", // night blue
+  onsColorBrandedTertiary: "#00a3a6", // aqua teal
+  onsColorBrandedSupporting: "#a8bd3a", // spring green
+  onsColorBrandedSupportingTint: "#a8bd3a", // spring green
+
+  // =============================================================================
+  // Assignment
+  // =============================================================================
+
+  // Header
+  onsColorHeader: "#206095",
+  onsColorHeaderNeutral: "#707071",
+  onsColorHeaderMasthead: "#fff",
+  onsColorHeaderMastheadInternal: "#003c57",
+  onsColorHeaderMastheadNeutral: "#222",
+  onsColorHeaderTitle: "#222",
+  onsColorHeaderNavigationLinks: "#222",
+  onsColorServiceLinks: "#fff",
+
+  // Banners, page
+  onsColorCtaBg: "#e9eff4",
+  onsColorBannerBg: "#f5f5f6",
+  onsColorBannerBgDark: "#e2e2e3",
+  onsColorPageLight: "#fff",
+  onsColorBannerBrowserBg: "#222",
+  onsColorHeroBg: "#e9eff4",
+  onsColorHeroBgDark: "#206095",
+
+  // Typography
+  onsColorText: "#222",
+  onsColorTextLight: "#707071",
+  onsColorTextInverse: "#fff",
+  onsColorTextBanner: "#222",
+  onsColorTextLink: "#206095",
+  onsColorTextLinkHover: "#003c57",
+  onsColorTextLinkActive: "#003c57",
+  onsColorTextLinkFocus: "#222",
+  onsColorTextInverseLink: "#fff",
+  onsColorTextInverseLinkHover: "#f5f5f6",
+  onsColorTextBannerLink: "#414042",
+  onsColorTextBannerLinkHover: "#222",
+  onsColorTextMetadata: "#222",
+
+  // Foundations
+  onsColorFocus: "#fbc900",
+  onsColorFocusDark: "#e2b500",
+  onsColorHighlight: "#f0f762",
+
+  // Borders
+  onsColorBorders: "#707071",
+  onsColorBordersIndent: "#bcbcbd",
+  onsColorBordersLight: "#bcbcbd",
+  onsColorBordersDocumentImage: "#e2e2e3",
+  onsColorBordersDocumentImageFocus: "#222",
+
+  // Placeholders
+  onsColorPlaceholder: "#707071",
+
+  // Form elements
+  onsColorButton: "#0f8243",
+  onsColorButtonSecondary: "#e2e2e3",
+  onsColorButtonShadow: "#073d20",
+  onsColorButtonHover: "#0c6b37",
+  onsColorButtonSecondaryShadow: "#707071",
+  onsColorButtonSecondaryHover: "#d5d5d6",
+  onsColorInputBorder: "#222",
+  onsColorInputBg: "#fff",
+
+  // Panels and status
+  onsColorInfo: "#206095",
+  onsColorInfoTint: "#e9eff4",
+  onsColorInfoVibrant: "#1f8fd8",
+  onsColorSuccess: "#0f8243",
+  onsColorSuccessTint: "#e7f3ec",
+  onsColorSuccessVibrant: "#10ca64",
+  onsColorErrors: "#d0021b",
+  onsColorErrorsTint: "#fae6e8",
+  onsColorErrorsVibrant: "#fd112d",
+  onsColorPending: "#fa6401",
+  onsColorPendingTint: "#fff0e6",
+  onsColorPendingVibrant: "#ff7b24",
+  onsColorDead: "#707071",
+  onsColorInstruction: "#fa6401",
+  onsColorInstructionTint: "#fff0e6",
+  onsColorTextDisabled: "#bcbcbd",
+  onsColorBorderDisabled: "#bcbcbd",
 };
